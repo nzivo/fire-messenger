54c69f4028bf236318492f17f98fa7800674086a
nzivo <johnnnzivo@gmail.com>
2024-08-21 02:01:27 +0300

    profile UI: Profile UI Done

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./ProfileUpdate.css";
+import assets from "../../assets/assets";
 
 const ProfileUpdate = () => {
-  return <div>ProfileUpdate</div>;
+  return (
+    <div className="profile">
+      <div className="profile-container">
+        <form>
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input type="file" id="avatar" accept=".png, .jpg, .jeg" hidden />
+            <img src={assets.avatar_icon} alt="" />
+            Upload profile image
+          </label>
+          <input type="text" placeholder="Full Name" required />
+          <textarea name="" placeholder="Profile bio" id="" required></textarea>
+          <button type="submit">Save</button>
+        </form>
+        <img className="profile-pic" src={assets.logo_icon} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default ProfileUpdate;
