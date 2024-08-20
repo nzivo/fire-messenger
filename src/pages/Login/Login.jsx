b6e7be87fb123882ea8a43e0321a8e3ec695acae
nzivo <johnnnzivo@gmail.com>
2024-08-20 23:35:48 +0300

    Auth page UI done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
+import assets from "../../assets/assets";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [currentState, setCurrentState] = useState("Register");
+  return (
+    <div className="login">
+      <img src={assets.logo_big} alt="" className="logo" />
+      <form className="login-form">
+        <h2>{currentState}</h2>
+        {currentState === "Register" ? (
+          <input
+            type="text"
+            placeholder="Username"
+            className="form-input"
+            required
+          />
+        ) : null}
+        <input
+          type="email"
+          placeholder="Email Address"
+          className="form-input"
+        />
+        <input type="password" placeholder="password" className="form-input" />
+        <button type="submit">
+          {currentState === "Register" ? "Sign Up" : "Sign In"}
+        </button>
+        <div className="login-term">
+          <input type="checkbox" className="" id="" />
+          <p>Agree to the terms of use & privacy policy</p>
+        </div>
+        <div className="login-forgot">
+          {currentState === "Register" ? (
+            <p className="login-toggle">
+              Already have an account?{" "}
+              <span onClick={() => setCurrentState("Login")}>Login Here</span>
+            </p>
+          ) : (
+            <p className="login-toggle">
+              Don&apos;t have an Account?{" "}
+              <span onClick={() => setCurrentState("Register")}>
+                Register Here
+              </span>
+            </p>
+          )}
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
