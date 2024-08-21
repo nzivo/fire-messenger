a80485c5115aea56bed4b04e0300b8e6ae34d05b
nzivo <johnnnzivo@gmail.com>
2024-08-21 17:06:03 +0300

    auth: Notification toast done

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Chat from "./pages/Chat/Chat";
 import ProfileUpdate from "./pages/ProfileUpdate/ProfileUpdate";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/chat" element={<Chat />} />
-      <Route path="/profile" element={<ProfileUpdate />} />
-    </Routes>
+    <>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/profile" element={<ProfileUpdate />} />
+      </Routes>
+    </>
   );
 };
 
