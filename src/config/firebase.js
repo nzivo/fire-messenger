0f891eca3043b4158c422425d300dfdabc2b9f39
nzivo <johnnnzivo@gmail.com>
2024-08-21 17:09:21 +0300

    auth: syntax error fixed

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -23,25 +23,25 @@ const db = getFirestore(app);
 
 // Firebase signup method
 const signup = async (username, email, password) => {
-    try {
-        const res = await createUserWithEmailAndPassword(auth,email,password);
-        const user = res.user;
-        await setDoc(doc(db,"users",user.uid),{
-            id:user.uid,
-            username:username.toLowerCase();
-            email,
-            name:"",
-            avatar:"",
-            bio:"Hey there, I am using fire message",
-            lastSeen:Date.now()
-        })
-        await setDoc(doc(db,"chats",user.uid),{
-            chatData:[]
-        })
-    } catch (error) {
-        console.error(error);
-        toast.error(error.code)
-    }
-}
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await setDoc(doc(db, "users", user.uid), {
+      id: user.uid,
+      username: username.toLowerCase(),
+      email,
+      name: "",
+      avatar: "",
+      bio: "Hey there, I am using fire message",
+      lastSeen: Date.now(),
+    });
+    await setDoc(doc(db, "chats", user.uid), {
+      chatData: [],
+    });
+  } catch (error) {
+    console.error(error);
+    toast.error(error.code);
+  }
+};
 
-export {signup}
+export { signup };
